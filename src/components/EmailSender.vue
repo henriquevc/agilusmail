5a9616702f2e37c77ad6fa59ab70391ae66a9519
henriquevc <henriquevc93@gmail.com>
2023-04-20 17:53:50 -0300

    adicionando lista de destinatarios

--- src/components/EmailSender.vue
+++ src/components/EmailSender.vue
@@ -4,7 +4,18 @@
     <div class="row q-col-gutter-md items-center">
       <div class="row col-8 q-col-gutter-md">
         <q-input outlined v-model="Email.Remetente" class="col-12" label="Remetente"></q-input>
-        <q-input outlined v-model="Email.Destinatarios[0].Endereco" class="col-12" label="Destinatários"></q-input>
+        <q-select
+          outlined
+          v-model="Email.Destinatarios"
+          :options="listaDestinatarios"
+          multiple
+          use-chips
+          use-input
+          stack-label
+          class="col-12"
+          label="Destinatários"
+          @new-value="criarDestinatario"
+        />
       </div>
       <div class="col-4">
         <q-btn @click="enviar" color="primary" icon="send">Enviar</q-btn>
@@ -14,13 +25,28 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { Email } from '../models/Email';
 
 const Email = ref({
   Remetente: 'comercial@agilus.com.br',
-  Destinatarios: [{Endereco: 'henrique@agilus.com.br'}]
+  Destinatarios: [{Endereco: 'henrique@agilus.com.br', label: 'henrique@agilus.com.br', value: 1}]
 })
+
+const listaDestinatarios = ref([])
+
+onMounted(() => {
+  listaDestinatarios.value.push({label: 'claudio@agilus.com.br', value: 2})
+  listaDestinatarios.value.push({label: 'allan@agilus.com.br', value: 3})
+  listaDestinatarios.value.push({label: 'clayton@agilus.com.br', value: 4})
+  listaDestinatarios.value.push({label: 'felippe@agilus.com.br', value: 5})
+  listaDestinatarios.value.push({label: 'renan@agilus.com.br', value: 6})
+})
+
+const criarDestinatario = (val) => {
+    let newId = listaDestinatarios.value[listaDestinatarios.value.length]
+    listaDestinatarios.value.push({label: val, value: newId})
+}
 
 const enviar = () => {
   console.log('clicou em enviou')

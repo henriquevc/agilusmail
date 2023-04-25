7138b059c31d8fc0630a8def071198b5259510d9
henriquevc <henriquevc93@gmail.com>
2023-04-25 18:13:00 -0300

    criando a edição e inclusão de lista

---
+++ src/pages/ListsDetail.vue
@@ -0,0 +1,12 @@
+<template>
+    <q-page padding>
+        <q-input v-model="list.name" label="Nome da lista"></q-input>
+        <q-input v-model="list.emails" type="textarea" rows="30"></q-input>
+    </q-page>
+</template>
+<script setup>
+const list = ref({
+    name: '',
+    emails: ''
+})
+</script>

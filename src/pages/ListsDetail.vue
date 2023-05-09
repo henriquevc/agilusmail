9999e12993e2b5a6bd8ab7fabf4225feae1dd1ba
Henrique <henriquevc93@gmail.com>
2023-05-09 11:47:59 -0300

    separando a lista de emails por ; e enter

--- src/pages/ListsDetail.vue
+++ src/pages/ListsDetail.vue
@@ -2,11 +2,11 @@
     <q-page padding>
         <div class="row justify-center q-gutter-lg q-mt-md q-mb-xl">
             <q-btn label="cancelar" flat color="negative" @click="cancel"></q-btn>
-            <q-btn label="salvar" color="positive"></q-btn>
+            <q-btn label="salvar" color="positive" @click="save"></q-btn>
         </div>
         <div class="row q-col-gutter-md">
             <q-input v-model="list.name" label="Nome da lista" outlined class="col-12" />
-            <q-input v-model="list.emails" label="Emails" type="textarea" rows="10" outlined class="col-12" hint="coloque os emails separados por virgula ou enter" />
+            <q-input v-model="emailsArray" label="Emails" type="textarea" rows="10" outlined class="col-12" hint="coloque os emails separados por virgula ou enter" />
         </div>
     </q-page>
 </template>
@@ -15,12 +15,21 @@ import {ref} from 'vue'
 import { useRouter } from 'vue-router'
 const list = ref({
     name: '',
-    emails: ''
+    emails: [] as string[]
 })
+const emailsArray = ref('')
 
 const router = useRouter()
 
 const cancel = () => {
     router.back()
+}
+
+const save = () => {
+  //separar os emails por ; e enter
+  list.value.emails = emailsArray.value
+    .split('\n')
+    .map(item => item.split(';'))
+    .reduce((acc, cur) => cur.concat(acc), []).filter(e => !!e)
 }
 </script>

b4cd2d0fe3279201a5b5a62fc4fa67b131c44247
Henrique <henriquevc93@gmail.com>
2023-05-12 19:30:14 -0300

    formatando tabela de listas de email

--- src/pages/ListsPage.vue
+++ src/pages/ListsPage.vue
@@ -4,15 +4,64 @@
     <div>
       <q-btn color="primary" label="Incluir Lista" @click="addList" />
 
-      <q-table class="q-mt-lg"></q-table>
+      <q-table
+        class="q-mt-lg"
+        :rows="rows"
+        :columns="columns"
+        row-key="name"
+      >
+        <template v-slot:body="props">
+          <q-tr :props="props" @click="onRowClick(props.row)" class="cursor-pointer">
+            <q-td key="name">{{ props.row.name }}</q-td>
+            <q-td key="actions" :props="props">
+              <q-btn icon="edit" flat round color="primary"></q-btn>
+              <q-btn icon="delete" flat round color="negative"></q-btn>
+            </q-td>
+          </q-tr>
+        </template>
+      </q-table>
     </div>
   </q-page>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { useRouter } from 'vue-router';
 const router = useRouter()
+
 const addList = () => {
   router.push('listDetail')
+}
+
+const columns = [
+  { name: 'name', required: true, label: 'Dessert (100g serving)', field: 'name', align: 'left', sortable: true },
+  { name: 'actions', label: '', field: 'actions', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const rows = [
+  {
+    name: 'Frozen Yogurt',
+  },
+  {
+    name: 'Ice cream sandwich',
+  },
+  {
+    name: 'Eclair',
+  },
+  {
+    name: 'Cupcake',
+  },
+  {
+    name: 'Gingerbread',
+  },
+  {
+    name: 'Jelly bean',
+  },
+  {
+    name: 'Lollipop',
+  },
+]
+
+const onRowClick = (item) => {
+  console.log(item)
 }
 </script>
